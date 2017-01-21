01b5addab6d7559731b85ddc76dff78a1612ee06
League <rhymeinparentheses@gmail.com>
2017-01-21 11:20:49 -0800

    Save

---
+++ src/EasyPainting.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class EasyPainting {
+	JFrame frame = new JFrame();
+public static void main(String[] args) {
+	EasyPainting paint = new EasyPainting();
+	paint.FrameVis();
+}
+public void FrameVis(){
+	frame.setVisible(true);
+}
+}

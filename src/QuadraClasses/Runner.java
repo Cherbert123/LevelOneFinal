42391e2368736dee912e5bc5a4f431701fc26ce9
najclark <league@WTS13.attlocal.net>
2017-01-22 11:25:54 -0800

    QuadraClasses

---
+++ src/QuadraClasses/Runner.java
@@ -0,0 +1,8 @@
+package QuadraClasses;
+
+public class Runner {
+Gymnast person = new Gymnast("Jerry");
+dragon fred = new dragon("fred",8000,"Fluffy");
+Classroom boring = new Classroom(10,90,80,900);
+Sausage sg = new Sausage(5,"Spicy");
+}
